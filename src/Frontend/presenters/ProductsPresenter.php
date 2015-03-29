c81df2e776f9ec1352415a89949c5b061eca85e8
Jakub Šanda <sanda.jakubhtc@gmail.com>
2015-03-29 17:45:41 +0200

    added missing functions and parameters

--- src/Frontend/presenters/ProductsPresenter.php
+++ src/Frontend/presenters/ProductsPresenter.php
@@ -91,6 +91,17 @@ class ProductsPresenter extends BasePresenter
             $this->template->accessoriescategory = $this->accessoriescategory;
             $this->template->accessories = $this->accessories;
             $this->template->product = $this->product;
+
+            $countOfCarouselPhotos = $this->em->getRepository('WebCMS\ProductreviewModule\Entity\Photo')->findBy(array(
+                'product' => $this->product,
+                'inCarousel' => 1
+            ));
+
+            $this->template->seoTitle = $this->product->getMetaTitle();
+            $this->template->seoDescription = $this->product->getMetaDescription();
+            $this->template->seoKeywords = $this->product->getMetaKeywords();
+            $this->template->countOfCarouselPhotos = count($countOfCarouselPhotos);
+            $this->template->form = $this->createComponentForm('form', $this, $this->actualPage);
             $this->template->setFile(APP_DIR . '/templates/productreview-module/Products/detail.latte');
         }
 
@@ -103,6 +114,79 @@ class ProductsPresenter extends BasePresenter
         $this->template->products = $this->products;
     }
 
+    public function createComponentForm($name, $context = null, $fromPage = null) 
+    {
+        if($context != null){
+            $form = new UI\Form();
+            $form->getElementPrototype()->action = $context->link('default', array(
+                'path' => $fromPage->getPath(),
+                'abbr' => $context->abbr,
+                'parameters' => array($context->product->getSlug()),
+                'do' => 'form-submit'
+            ));
+            $form->setTranslator($context->translator);
+            $form->setRenderer(new BootstrapRenderer);
+            
+            $form->getElementPrototype()->class = 'form-horizontal contact-agent-form';
+            
+        }else{
+            $form = new UI\Form();
+            $form->getElementPrototype()->action = $this->link('default', array(
+                'path' => $this->actualPage->getPath(),
+                'abbr' => $this->abbr,
+                'parameters' => array($this->product->getSlug()),
+                'do' => 'form-submit'
+            ));
+            $form->setTranslator($this->translator);
+            $form->setRenderer(new BootstrapRenderer);
+            
+            $form->getElementPrototype()->class = 'form-horizontal contact-agent-form';
+        }
+        $form->addText('name', 'Name:')->setRequired();
+        $form->addText('email', 'E-mail:')
+            ->addRule(UI\Form::EMAIL, 'Email is not valid')
+            ->setRequired();
+        $form->addTextArea('text', 'Text:')->setRequired();
+        //if($this->product){
+            $form->addHidden('productId', $this->product->getId());
+        //}
+        $form->addSubmit('submit', 'Send')->setAttribute('class', 'btn btn-success');
+        $form->onSuccess[] = callback($this, 'formSubmitted');
+        return $form;
+    }
+    
+    public function formSubmitted($form)
+    {
+        $values = $form->getValues();
+        $mail = new \Nette\Mail\Message;
+        $infoMail = $this->settings->get('Info email', 'basic', 'text')->getValue();
+        $mail->addTo($infoMail);
+        
+        $domain = str_replace('www.', '', $this->getHttpRequest()->url->host);
+        
+        if($domain !== 'localhost') $mail->setFrom('no-reply@' . $domain);
+        else $mail->setFrom('no-reply@test.cz'); // TODO move to settings
+        $product = $this->repository->find($values->productId);
+        $mailBody = '<h2>Poptávaný produkt: '.$product->getName().'</h2>';
+        $mailBody .= '<p><strong>Jméno: </strong>'.$values->name.'</p>';
+        $mailBody .= '<p><strong>Email: </strong>'.$values->email.'</p>';
+        $mailBody .= '<p><strong>Text zprávy: </strong>'.$values->text.'</p>';
+        $mail->setSubject('Poptávka produktu '.$product->getName());
+        $mail->setHtmlBody($mailBody);
+        try {
+            $mail->send();  
+            $this->flashMessage('Form has been sent', 'success');
+        } catch (\Exception $e) {
+            $this->flashMessage('Cannot send email.', 'danger');                    
+        }
+       
+        $httpRequest = $this->getContext()->getService('httpRequest');
+        $url = $httpRequest->getReferer();
+        $url->appendQuery(array(self::FLASH_KEY => $this->getParam(self::FLASH_KEY)));
+        $this->redirectUrl($url->absoluteUrl);
+        
+    }
+
     public function productsBox($context, $fromPage)
     {
         $template = $context->createTemplate();
@@ -110,22 +194,19 @@ class ProductsPresenter extends BasePresenter
             'hide' => false,
             'page' => $fromPage,
             'homepage' => true
-        ));
-
+        ), array('productOrder' => 'ASC'));
         $template->productPage = $context->em->getRepository('WebCMS\Entity\Page')->findOneBy(array(
             'moduleName' => 'Productreview',
-            'presenter' => 'Products'
+            'presenter' => 'Products',
+            'language' => $fromPage->getLanguage()
         ));
-
         $template->link = $context->link(':Frontend:Productreview:Products:default', array(
             'id' => $fromPage->getId(),
             'path' => $fromPage->getPath(),
             'abbr' => $context->abbr
         ));
-
         $template->abbr = $context->abbr;
         $template->setFile(APP_DIR . '/templates/productreview-module/Products/productsBox.latte');
-
         return $template;  
     }
 
